e8d8bf1eb2981d60c64946a58b4f2e1195734166
Justyn Hunter <10092578+justynhunter@users.noreply.github.com>
2023-11-18 19:45:25 -0600

    Feature/array2d select (#1)
    * update to .net 8
    
    * add extension method for 2d array
    
    * rename to select and use it
    
    * get rid of null warnings
    
    * revert to .net 6
    
    ---------
    
    Co-authored-by: Justyn Hunter <justynhunter@gmail.com>

---
+++ GameOfLife/Util/Array2D.cs
@@ -0,0 +1,16 @@
+public static class Array2D
+{
+  public static U[,] Select<T,U>(this T[,] values, Func<int,int,T,U> fn)
+  {
+    var result = new U[values.GetLength(0),values.GetLength(1)];
+    for (int x = 0; x < values.GetLength(0); x++)
+    {
+      for (var y = 0; y < values.GetLength(1); y++)
+      {
+        result[x,y] = fn(x, y, values[x, y]);
+      }
+    }
+
+    return result;
+  }
+}
